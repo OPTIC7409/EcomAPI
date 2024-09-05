9c05eb260bafbda73a28027a7a3b92cbe2b7b89a
OPTIC7409 <79322893+OPTIC7409@users.noreply.github.com>
2024-09-05 13:43:47 +0100

    Types and utils + Register Endpoint

--- service/user/routes.go
+++ service/user/routes.go
@@ -3,10 +3,13 @@ package user
 import (
 	"net/http"
 
+	"github.com/OPTIC7409/EcomApi/types"
+	"github.com/OPTIC7409/EcomApi/utils"
 	"github.com/gorilla/mux"
 )
 
 type Handler struct {
+	store *types.UserStore
 }
 
 func NewHandler() *Handler {
@@ -25,5 +28,12 @@ func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
+	// get json payload
+	// check if user exists
+	// if it doesnt create the new users
+	var payload types.RegisterUserPayload
+	if err := utils.ParseJSON(r, payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+	}
 
 }
---
+++ utils/utils.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+)
+
+func ParseJSON(r *http.Request, payload any) error {
+	if r.Body == nil {
+		return errors.New("request body is empty")
+	}
+	return json.NewDecoder(r.Body).Decode(payload)
+}
+
+func RespondJSON(w http.ResponseWriter, status int, payload any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(payload)
+}
+
+func WriteError(w http.ResponseWriter, status int, err error) {
+	RespondJSON(w, status, map[string]string{"error": err.Error()})
+}
